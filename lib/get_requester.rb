c12bfd801a51b42c14fdbcb1fb7286fae244d999
Trayshawn <traytwebb14@yahoo.com>
2020-11-27 20:45:45 -0600

    commit tree 35d1604a9d82886373c87eb31206ccbecc0feb4d

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,31 @@
 # Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+require 'pry'
+
+
+class GetRequester
+
+    attr_accessor :url
+
+    URL = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+  
+    def initialize(url)
+        @url = url
+    end
+        
+    def get_response_body
+        uri = URI.parse(URL)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+  
+    def parse_json
+        programs = JSON.parse(self.get_response_body)
+        programs.collect do |request|
+          request  
+        end
+      end 
+  
+  end
